d6e94942753c1d0c01d6db4efb8ce936d5a8f92d
Navita Rajbhar <rajbharnavita@gmail.com>
2025-06-19 23:24:16 -0700

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+async function connectMongoDb() {
+  return mongoose
+    .connect(
+      "mongodb+srv://navita:Rnavita02@cluster0.f1auc.mongodb.net/social?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+}
+module.exports = connectMongoDb;
